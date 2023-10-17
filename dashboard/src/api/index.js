c5c830fd15bba836ac37bf2816b04b7f37c3384d
Abdun Noor Faruki Biswas <andunnoorfarukibiswas6761@gmail.com>
2023-10-17 20:21:06 +0530

    some changes in api/inde.js

--- dashboard/src/api/index.js
+++ dashboard/src/api/index.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 const baseURL = import.meta.env.VITE_API_URL
+const accessToken = localStorage.getItem("accessToken")
 const api = axios.create({
     baseURL,
     headers:{
         "Access-Control-Allow-Origin": "*",
-        Authorization:`Bearer ${localStorage.getItem("accessToken")}`
+        Authorization:`Bearer ${accessToken}`
     },
 })
 export default api
